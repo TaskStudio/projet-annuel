223304f92f3220fdd97bd40bf476bbfc5c161e3e
Anakaryna <78374876+Anakaryna@users.noreply.github.com>
2024-02-28 23:40:00 +0100

    Fix entity to ground

--- Assets/Scripts/Entities/EntityMover.cs
+++ Assets/Scripts/Entities/EntityMover.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class EntityMover : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public LayerMask groundLayer;
     private Vector3 targetPosition;
     private bool isMoving;
     
     public float raycastLength = 5f;
     public float collisionAvoidanceTurn = 0.5f;
+    
+    private float groundCheckDistance;
+
+    private void Start()
+    {
+        Collider collider = GetComponent<Collider>();
+        groundCheckDistance = collider.bounds.extents.y;
+    }
 
     public void SetTargetPosition(Vector3 position)
     {
@@ -26,23 +35,35 @@ public class EntityMover : MonoBehaviour
     private void MoveTowardsTarget()
     {
         Vector3 directionToTarget = (targetPosition - transform.position).normalized;
-        Vector3 newPosition = transform.position + directionToTarget * moveSpeed * Time.deltaTime;
-
+        directionToTarget.y = 0;
+        
         RaycastHit hit;
-        // Vérifie s'il y a un obstacle dans la direction du mouvement
-        if (Physics.Raycast(transform.position, directionToTarget, out hit, raycastLength))
+        Vector3 currentPosition = transform.position + Vector3.up * groundCheckDistance;
+        if (Physics.SphereCast(currentPosition, groundCheckDistance, directionToTarget, out hit, raycastLength, ~groundLayer))
         {
             Vector3 hitNormal = hit.normal;
-            hitNormal.y = 0; 
+            hitNormal.y = 0;
             directionToTarget = Vector3.RotateTowards(directionToTarget, hitNormal, collisionAvoidanceTurn, 0.0f);
         }
-        
+
+        // Déplacement de l'unité dans la direction ajustée
         transform.position += directionToTarget * moveSpeed * Time.deltaTime;
-        transform.rotation = Quaternion.LookRotation(directionToTarget);
+
+        // Raycast vers le bas pour déterminer la position y en fonction du sol
+        if (Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, out hit, groundCheckDistance + 0.2f, groundLayer))
+        {
+            transform.position = new Vector3(transform.position.x, hit.point.y + groundCheckDistance, transform.position.z);
+        }
         
         if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
         {
             isMoving = false;
+        }
+        
+        if (directionToTarget != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(directionToTarget, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, moveSpeed * Time.deltaTime);
         }
     }
 }
