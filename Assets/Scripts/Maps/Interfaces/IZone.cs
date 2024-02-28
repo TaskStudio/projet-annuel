1228f57b6e6edf1b8548f1bf14e949e0deae0163
Onsraa <truongteddy306@icloud.com>
2024-02-28 23:33:27 +0100

    Create IZone interface

---
+++ Assets/Scripts/Maps/Interfaces/IZone.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Maps.Interfaces
+{
+    public class IZone : MonoBehaviour
+    {
+        Vector3 Position { get; }
+        string Name { get; }
+    }
+}
+
