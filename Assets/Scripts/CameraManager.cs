ac992f8744debc7d5e4c0c04e025f004e8b37e7b
Onsraa <truongteddy306@icloud.com>
2024-03-02 16:30:08 +0100

    Add CameraManager class

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CameraManager : MonoBehaviour
+{
+    private CinemachineVirtualCamera _virtualCamera;
+    
+    public bool freeView = true;
+    
+    private Transform _selectedObjectTransform;
+    private Transform _cameraSystemTransform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();    
+        _cameraSystemTransform = FindObjectOfType<CameraSystem>().transform;            
+        UpdateView();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Y)) return;
+        UpdateView();
+    }
+
+    void UpdateView()
+    {
+        if (freeView)
+        {
+            _virtualCamera.LookAt = _cameraSystemTransform;
+            _virtualCamera.Follow = _cameraSystemTransform;
+        }
+        else
+        {
+            _virtualCamera.LookAt = _selectedObjectTransform;
+            _virtualCamera.Follow = _selectedObjectTransform;        
+        }
+        freeView = !freeView;
+    }
+
+    public void UpdateLastSelectedObject(Transform selectedObject)
+    {
+        _selectedObjectTransform = selectedObject;
+        freeView = true;
+    }
+}
