59d78c97c6fbb1787e2840fe61497b14267d9242
Anakaryna <78374876+Anakaryna@users.noreply.github.com>
2024-03-04 22:16:43 +0100

    Add delay to entity spawn

--- Assets/Scripts/Entities/EntitySpawner.cs
+++ Assets/Scripts/Entities/EntitySpawner.cs
@@ -1,24 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class EntitySpawner : MonoBehaviour {
     public GameObject entityPrefab; 
-    
-    void Start() {
-        
-        Vector3 startPosition = new Vector3(0, 1, 0); 
-        SpawnEntity(startPosition);
-    }
-    
+    public float spawnDelay = 2f;
+    private bool isSpawning = false;
+
     void Update() {
-        
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isSpawning)
         {
-            Vector3 randomPosition = new Vector3(Random.Range(-10, 10), 1, Random.Range(-10, 10)); // Set y to 1
-            SpawnEntity(randomPosition);
+            StartCoroutine(SpawnEntityWithDelay());
         }
     }
 
-    void SpawnEntity(Vector3 position) {
-        Instantiate(entityPrefab, position, Quaternion.identity);
+    IEnumerator SpawnEntityWithDelay() {
+        isSpawning = true;
+        while (true)
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(-10, 10), 1, Random.Range(-10, 10));
+            Instantiate(entityPrefab, randomPosition, Quaternion.identity);
+            yield return new WaitForSeconds(spawnDelay);
+        }
     }
 }
